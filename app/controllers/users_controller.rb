810c422693641b796d14925ba84106f2682ec502
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-09-14 10:26:03 +0000

    [Update]応用課題2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @users = User.all
     @book = Book.new
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
@@ -19,11 +19,9 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
-      @books = @user.books
-      @book = Book.new
-      render :show
+      render :edit
     end
   end
 
@@ -37,6 +35,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     unless @user == current_user
       redirect_to user_path(current_user)
-    end 
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,8 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
-
+  validates :introduction, length: {maximum: 50}
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-
-
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
