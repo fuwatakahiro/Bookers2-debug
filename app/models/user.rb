d6f9832f3f653fdb1216c64c2baa6562ed1579e7
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-10-01 06:23:45 +0000

    [Finish] number of views

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
    has_many :user_rooms, dependent: :destroy
    has_many :chats, dependent: :destroy
    has_many :rooms, through: :user_rooms, dependent: :destroy
+   has_many :view_counts, dependent: :destroy
   # フォローをした、されたの関係
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
